863349dc9830c71682ed173562a272f735567946
Mark Jordan <mjordan@sfu.ca>
2014-10-05 16:21:45 -0700

    Enabled bottle script.

--- tests.py
+++ tests.py
@@ -5,8 +5,8 @@ import MySQLdb.cursors
 import sys
 
 def test_requests():
-    # r = requests.get('http://localhost/hello')
-    r = requests.get('http://localhost')
+    r = requests.get('http://localhost/hello')
+    # r = requests.get('http://localhost')
     assert r.status_code == 200
 
 def test_mysql():
